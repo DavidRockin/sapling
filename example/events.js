0251aa94af10b9f44165f65ee776fb26c755b2a5
Dave <DavidRockin@users.noreply.github.com>
2018-07-03 14:10:28 -0400

    adding example of events

---
+++ example/events.js
@@ -0,0 +1,49 @@
+const Sapling = require('../src/sapling').app({
+	port: 8082
+});
+
+/**
+ * Routes
+ */
+
+// any requests after this will get this data
+Sapling.use((req,res) => {
+	console.log(`${req.client.uuid} >> default`);
+	res.defaultData = 'sample';
+})
+
+Sapling.use('test', (req,res) => {
+	console.log(`${req.client.uuid} >> test route`);
+	res.test = true
+	return true;
+})
+
+Sapling.use('example', (req,res) => {
+	console.log(`${req.client.uuid} >> example route`);
+	res.action = 'example-response';
+	res.data   = 'OK';
+})
+
+/**
+ * Custom events
+ */
+
+// listen in for when a client connects
+Sapling.on('clientConnect', client => {
+	console.log(`New connection from ${client._socket.remoteAddress}`);
+
+	// we could assign custom client identifiers
+	client.uuid = 'example-' + new Date().getTime();
+});
+
+// listen for when a client disconnects
+Sapling.on('clientClose', (client, code) => {
+	console.log(`Lost connection from ${client.uuid}`);
+
+	// could do stuff here, broadcast info, save user data etc
+});
+
+// if you're feeling crazy enough to listen for all raw client messages, you could...?
+Sapling.on('clientMessage', (client, message) => {
+	console.log(`Message from ${client.uuid} >> ${message}`);
+});
